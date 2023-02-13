eb8c8cc77db03539b71b9bd1b2785ea14b54ac07
Sandeep mainali <amanshrestha284@gmail.com>
2023-02-13 21:46:29 +0545

    14Feb

--- Assignment/Business.java
+++ Assignment/Business.java
@@ -1,11 +1,10 @@
 public class Business{
-    public static void main(String[] args){
-        System.out.println("Organization name: Mirror Store");
+    public static void main(String[] xyz){
+        System.out.println("Organization name: Mirror Store\n");
         System.out.println("Address:");
         System.out.println("---- Bhaktapur");
         System.out.println("---- Zip Code:44600");
-        System.out.println("---- Country:Nepal");
-        
+        System.out.println("---- Country:Nepal\n");
          System.out.println("Contact Information:");
          System.out.println("----Mobile Number: 98564654344");
          System.out.println("----Email:Mirrorstore@gmail.com");
