14f567cd3dbf02a2472a5c2c8df269d698255f62
Sandeep mainali <amanshrestha284@gmail.com>
2023-02-13 10:44:33 +0545

    Java

---
+++ Bio/My.java
@@ -0,0 +1,6 @@
+public class My{
+    public static void main(String[] abc){
+        System.out.println("Name:Sandeep Mainali");
+        System.out.println("Age:23 ");
+        System.out.println("class:Bachelor ");   }
+}
---
+++ Java-kist/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+           System.out.print("I am ");
+              System.out.println("Sandeep Mainali");
+                 System.out.println("Welcomem To New World");
+
+    }
+
+}
+
+
